21ba7bff6a27ea79499f005d766757f76485ce3d
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-08-23 16:47:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;DI1yxQLT#X3od0JpeK1Z-jP2hKrxRzwzj8aImnTG4palctvqK2Kw", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "94705104830", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
